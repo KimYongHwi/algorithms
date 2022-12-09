d19c55ad271b2cde0fa1c004b6fbf53dce3ccf2e
Kim-Yonghwi <yhkim@seoulstore.com>
2022-12-10 00:07:00 +0900

    feat: Codility TimeComplexity

---
+++ Codility/java/src/main/java/com/codility/lesson/timecomplexity/PermMissingElem.java
@@ -0,0 +1,15 @@
+package com.codility.lesson.timecomplexity;
+
+import java.util.Arrays;
+
+public class PermMissingElem {
+    public int solution(int[] A) {
+        A = Arrays.stream(A).sorted().toArray();
+
+        for (int i = 0; i < A.length; i++) {
+            if (i + 1 != A[i]) return i + 1;
+        }
+
+        return A.length + 1;
+    }
+}
